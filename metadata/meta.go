ec9f2a0b0032d4ef00d1b6be49084c535ddf6354
Royal Bhati <royal240596@gmail.com>
2020-08-13 11:39:29 +0530

    :fire: done

---
+++ metadata/meta.go
@@ -0,0 +1,21 @@
+package metadata
+
+import (
+	"mime"
+	"net/http"
+	"path/filepath"
+)
+
+func GetFileName(resp *http.Response) string {
+	contentDisposition := resp.Header.Get("Content-Disposition")
+	if contentDisposition != "" {
+		_, params, err := mime.ParseMediaType(contentDisposition)
+
+		if err != nil {
+			panic(err)
+		}
+		return params["filename"]
+	}
+	filename := filepath.Base(resp.Request.URL.Path)
+	return filename
+}
